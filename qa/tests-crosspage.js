6d9fbec91b0c4a22a7334a1cf7adaba0328bdd96
Thomas Babu <tommy.developer@gmail.com>
2014-12-27 17:04:37 +0800

    fixed 404 error for the two new pages and also added the crosspage testing

---
+++ qa/tests-crosspage.js
@@ -0,0 +1,48 @@
+/*
+*  Creating crosspage testing using zomibe headless browser
+*/
+
+var Browser = require('zombie'),
+	assert  = require('chai').assert;
+
+var browser;
+
+suite('Cross-Page Tests', function(){
+	setup(function(){
+		browser = new Browser();
+	});
+
+	test('requesting a group rate quote from the hood river tour page ' +
+		  'populate the hidden referrer field correctly', function(done) {
+
+	  	var referrer = 'http://localhost:3000/tours/hood-river';
+	  	browser.visit(referrer, function(){
+	  		browser.clickLink('.requestGroupRate', function(){
+	  			assert(browser.field('referrer').value === 'referrer');
+	  			done();
+	  		});
+	  	});
+	});
+
+	test('requesting a group rate from the oregon coast tour page should ' + 
+		'populate the referrer field', function(done) {
+
+		var referrer = 'http://localhost:3000/tours/oregon-coast';
+		browser.visit(referrer, function(){
+			browser.clickLink('.requestGroupRate', function(){
+				assert(browser.field('referrer').value === 'referrer');
+				done();
+			});
+		});
+	});
+
+	test('visiting the "request group rate" page directly should result '+
+		'in an empty referrer filed', function(done){
+		browser.visit('http://localhost:3000/tours/reques-group-rate', function(){
+			assert(browser.field('referrer').value === '');
+			done();
+		});	
+	});
+	
+		
+});
