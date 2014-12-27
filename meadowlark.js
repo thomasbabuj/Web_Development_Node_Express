44a7d7e79598aad46502c97c690d6f36b3dd9596
Thomas Babu <tommy.developer@gmail.com>
2014-12-27 12:59:22 +0800

    added a middleware to detect querysting

--- meadowlark.js
+++ meadowlark.js
@@ -21,6 +21,12 @@ app.set('view engine', 'handlebars');
 
 app.set('port', process.env.PORT || 3000);
 
+//Use middleware to detect test=1 in the querystring
+app.use(function(req, res, next) {
+	res.locals.showTests = app.get('env') !== 'production' && req.query.test === '1';
+	next();
+});
+
 //Adding a middleware to load static files and views
 app.use(express.static(__dirname + '/public'));
 
